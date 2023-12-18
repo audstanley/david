52256d8a9815d1800b5be4d774a57a0a0bcb39ed
Richard Stanley <audstanley@gmail.com>
2023-12-18 13:14:07 -0800

    almost forgot these

--- cmd/david/main.go
+++ cmd/david/main.go
@@ -18,20 +18,35 @@ func main() {
 	flag.StringVar(&configPath, "config", "", "Path to configuration file")
 	flag.Parse()
 
-	config := app.ParseConfig(configPath)
-
 	// Set formatter for logrus
-	formatter := &log.TextFormatter{}
-	log.SetFormatter(formatter)
+	ProductionFormatter := &log.JSONFormatter{}
+	NonProductionFormatter := &log.TextFormatter{}
+	log.SetFormatter(ProductionFormatter)
+	log.SetLevel(log.DebugLevel)
+
+	config := app.ParseConfig(configPath)
 
 	// Set formatter for default log outputs
 	logger := log.New()
-	logger.Formatter = formatter
+	if config.Log.Production {
+		logger.Formatter = ProductionFormatter
+		log.WithField("prdouction", config.Log.Production).Debug("Production mode enabled")
+	} else {
+		logger.Formatter = NonProductionFormatter
+		log.WithField("prdouction", config.Log.Production).Debug("Production mode disabled")
+		log.SetFormatter(NonProductionFormatter)
+	}
+	if config.Log.Debug {
+		log.WithField("debug", config.Log.Debug).Debug("Debug mode enabled")
+	} else {
+		log.WithField("debug", config.Log.Debug).Debug("Debug mode has now been disabled from config")
+		log.SetLevel(log.InfoLevel)
+	}
 	writer := logger.Writer()
 	defer writer.Close()
 	syslog.SetOutput(writer)
 
-	wdHandler := &webdav.Handler{
+	wdHandler := webdav.Handler{
 		Prefix: config.Prefix,
 		FileSystem: &app.Dir{
 			Config: config,
@@ -46,7 +61,7 @@ func main() {
 
 	a := &app.App{
 		Config:  config,
-		Handler: wdHandler,
+		Handler: &wdHandler,
 	}
 
 	http.Handle("/", wrapRecovery(app.NewBasicAuthWebdavHandler(a), config))
@@ -76,8 +91,12 @@ func wrapRecovery(handler http.Handler, config *app.Config) http.Handler {
 			if err := recover(); err != nil {
 				switch t := err.(type) {
 				case string:
+					log.Printf("panic type: %T, value: %v", err, err)
+					log.WithFields(log.Fields{"error": err, "writer": w}).Warn("An error occurred handling a webdav request")
 					log.WithError(errors.New(t)).Error("An error occurred handling a webdav request")
 				case error:
+					log.Printf("panic type: %T, value: %v", err, err)
+					log.WithFields(log.Fields{"error": err, "writer": w}).Warn("An error occurred handling a webdav request")
 					log.WithError(t).Error("An error occurred handling a webdav request")
 				}
 			}
